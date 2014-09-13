34bfed4ca2b25b92e1b9339ffab47f8d09c6bbd2
Anders Einar Hilden <hildenae@gmail.com>
2014-09-13 19:28:02 +0200

    Drop the next-prev-function, display continent, country and city.

--- select-server.sh
+++ select-server.sh
@@ -1,20 +1,19 @@
-HOSTNAME=$(echo "next
-Next server in list
-prev
-Same as last time"  | cat - serverlist.conf | \
+#! /bin/bash
+SERVERLIST="serverlist.conf"
+if [ "$1" != "" ]; then
+SERVERLIST=$1
+fi 
+HOSTNAME=$(cat $SERVERLIST | \
 zenity --list --title "Choose VPN server" \
---text "Choose the VPN server to connect through:" \
---column "device" --column "Name" --print-column=1 \
---hide-column=1 2>/dev/null | cut -d'|' -f1)
-echo $HOSTNAME
-NEXT=$(head -1 serverlist.conf)
-PREV=$(tail -2 serverlist.conf)
-echo "pre serverllist"
-MSG=$(grep -A 1 $HOSTNAME serverlist.conf | \
-tr '\n' '\t' | \
-sed -e 's/\t/ (/' -e 's/\t/)/')
-echo "post serverlist"
+--text "Choose the VPN server to connect through:                                                                " \
+--column "Continent" --column "Country" --column "City" --column "Address" --print-column=4 \
+ 2>/dev/null | cut -d'|' -f1)
+if [ "${HOSTNAME}" != "" ]; then
+MSG=$(grep -B 3 $HOSTNAME $SERVERLIST | head -3 | \
+awk 'FNR == 1 {C=$0} FNR == 2{ printf "%s, %s, ", C,$0 } END{print $0 }') 
 zenity --question --title="Verify VPN server" \
---text="Are you sure you wish to connect to:\n$MSG"
+--text="Are you sure you wish to connect to:\n$HOSTNAME\n($MSG)"
 echo $?
-#0 = true 1 = false
+else
+echo "Didn't select a server, assume cancel"
+fi
